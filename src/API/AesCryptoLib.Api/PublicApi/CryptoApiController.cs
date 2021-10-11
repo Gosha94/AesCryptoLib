6575c39771eef966f8821c933bcec6daf19da749
Gosha94 <gerizch@rambler.ru>
2021-10-11 12:37:42 +0300

    create crypto controller

---
+++ src/API/AesCryptoLib.Api/PublicApi/CryptoApiController.cs
@@ -0,0 +1,20 @@
+﻿namespace AesCryptoLib.Api.PublicApi
+{
+    /// <summary>
+    /// Контроллер для работы с AES шифрованием
+    /// </summary>
+    public sealed class CryptoApiController
+    {
+        /// <summary>
+        /// Метод получения расшифрованных данных
+        /// </summary>
+        /// <returns></returns>
+        public string GetDeclassifiedData()
+        {
+
+        }
+
+        public 
+
+    }
+}
