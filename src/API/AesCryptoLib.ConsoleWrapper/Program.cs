fef84b65e41308ab09e9bbc360c0fe7686ae6304
Katuha-ha <katy_04_94@mail.ru>
2021-10-12 00:26:38 +0300

    Удаление проекта

--- src/API/AesCryptoLib.ConsoleWrapper/Program.cs
+++ src/API/AesCryptoLib.ConsoleWrapper/Program.cs
@@ -6,6 +6,11 @@ namespace AesCryptoLib.ConsoleWrapper
     {
         static void Main(string[] args)
         {
+            var configurator = new StartupDependencyConfigurator();
+            configurator.RegisterAllInstancesInApplication();
+
+
+
             // Arrange
             var testCorrectFilePath = "D:\\UkTransmitterConfig\\JsonSecret\\TestUserData.json";
             var expectedSecretValue = "someTestData";
