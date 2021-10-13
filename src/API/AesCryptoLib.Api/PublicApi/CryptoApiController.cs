0852070a991c0c3bae1fbf2f36bad90315300d94
Gosha94 <gerizch@rambler.ru>
2021-10-14 01:14:39 +0300

    delete unised projects

--- src/API/AesCryptoLib.Api/PublicApi/CryptoApiController.cs
+++ src/API/AesCryptoLib.Api/PublicApi/CryptoApiController.cs
@@ -1,6 +1,6 @@
-﻿using AesCryptoLib.Api.Config;
-using AesCryptoLib.Core.Contracts;
-using AesCryptoLib.JsonParser.Helpers;
+﻿using JsonConfigParser.Api.PublicAPI;
+using JsonConfigParser.Core.Contracts;
+using JsonConfigParser.Core.FileConfigs;
 using AesCryptoLib.BusinessLogic.Services;
 
 namespace AesCryptoLib.Api.PublicApi
@@ -9,18 +9,15 @@ namespace AesCryptoLib.Api.PublicApi
     /// <summary>
     /// Контроллер для работы с AES шифрованием
     /// </summary>
-    public sealed class CryptoApiController : IAesController
+    public sealed class CryptoApiController
     {
 
         #region Private Fields
 
-        private readonly StartupDependencyConfigurator _configurator;
-        private readonly FileSearchService _fileService;
-
-        private readonly IReadOnlyUserDataRepository _jsonFileRepository;
-        private readonly IFileConfiguration _jsonFileConfiguration;
+        private IFileConfiguration _jsonConfiguration;
 
         private AesCryptoService _cryptoService;
+        private JsonParsingApi _jsonParsingApi;
 
         #endregion
 
@@ -30,15 +27,7 @@ namespace AesCryptoLib.Api.PublicApi
         /// Конструктор по умолчанию
         /// </summary>
         public CryptoApiController()
-        {
-            this._configurator = new StartupDependencyConfigurator();
-            this._configurator.RegisterAllInstancesInApplication();
-
-            this._jsonFileRepository = _configurator.GetRepositoryInstance();
-            this._jsonFileConfiguration = _configurator.GetJsonConfigurationInstance();
-
-            this._fileService = new FileSearchService(this._jsonFileConfiguration);
-        }
+        { }
 
         #endregion
 
@@ -51,13 +40,10 @@ namespace AesCryptoLib.Api.PublicApi
         /// <returns></returns>
         public string GetDeclassifiedData(string mysteryString)
         {
-            var secret = GetSecretFromRepository();
-
+            var secret = GetSecretFromJsonParsingApi();
             this._cryptoService = new AesCryptoService(secret);
             var encryptedData = mysteryString;
-
             var resultData = this._cryptoService.Decrypt(encryptedData);
-
             return resultData;
         }
 
@@ -68,13 +54,10 @@ namespace AesCryptoLib.Api.PublicApi
         /// <returns></returns>
         public string GetMysteryData(string openString)
         {
-            var secret = GetSecretFromRepository();
-
+            var secret = GetSecretFromJsonParsingApi();
             this._cryptoService = new AesCryptoService(secret);
             var decryptedData = openString;
-
             var resultData = this._cryptoService.Encrypt(decryptedData);
-
             return resultData;
         }
 
@@ -82,10 +65,14 @@ namespace AesCryptoLib.Api.PublicApi
 
         #region Private Methods
 
-        private string GetSecretFromRepository()
+        /// <summary>
+        /// Метод получения секретного ключа из JSON файла конфигурации
+        /// </summary>
+        /// <returns></returns>
+        private string GetSecretFromJsonParsingApi()
         {
-            var pathToJsonConfig = this._fileService.GetFullPathToJsonFile();
-            var result = this._jsonFileRepository.GetSecretFromSource(pathToJsonConfig);
+            this._jsonConfiguration = new JsonSaltFileConfiguration();
+            var result = this._jsonParsingApi.GetSecretJsonData(this._jsonConfiguration);
             return result;
         }
 
